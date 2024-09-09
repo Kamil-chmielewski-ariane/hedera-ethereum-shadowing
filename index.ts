ff2d45bd912b57254d9484ebea6e52314c64e2ed
mmyslariane <michal.myslinski@arianelabs.com>
2024-09-09 16:50:32 +0200

    creating accounts

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ import {
     AccountId,
     AccountCreateTransaction,
 } from "@hashgraph/sdk";
+import {base32} from 'rfc4648';
 
 type accountType = Record<string, {balance: string}>;
 
@@ -13,28 +14,40 @@ const accountsMapping = getAllFrontierData() // Record<string, {balance: string}
 
 const node = {"127.0.0.1:50211": new AccountId(3)}
 
-async function createHederaAccount(options : accountType ) {
+async function createHederaAccount(accounts : accountType ) {
+    
+
     const client = Client.forNetwork(node).setMirrorNetwork("127.0.0.1:5600");
     client.setOperator(new AccountId(2), '302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137')
 
     console.log('Creating new account...');
+    for (const key in accounts) {
+        const balance = accounts[key].balance;
+        const privateKey = PrivateKey.generateECDSA();
+        const longAccountId = await transformPublicKeyToAccountId(key)
+        const signTransaction = await new AccountCreateTransaction()
+            .setKey(privateKey)
+            .setAlias(longAccountId)
+            .setInitialBalance(new Hbar(parseInt(balance) / 1000000000000))
+            .freezeWith(client)
+            .sign(PrivateKey.fromString('302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137'));
+        const newAccount = await signTransaction.execute(client);
+        
+        const receipt = await newAccount.getReceipt(client);
+        const newAccountId = receipt.accountId;
 
-    const newAccount = await new AccountCreateTransaction()
-        .setKey(PrivateKey.fromString('302e020100300506032b65700422042091132178e72057a1d7528025956fe39b0b847f200ab59b2fdd367017f3087137'))
-        .setInitialBalance(new Hbar(1))
-        .execute(client)
+        console.log('receipt', receipt);
 
-    const receipt = await newAccount.getReceipt(client);
-
-    const newAccountId = receipt.accountId;
-
-    console.log('receipt', receipt)
-
-    console.log("The new account ID is " + newAccountId);
-
-    return newAccountId;
+        console.log("The new account ID is " + newAccountId);
+    }
 }
 
 createHederaAccount(accountsMapping).then((elem) => {
     console.log('createHedera account data', elem);
 }).catch((error) => console.log(error));
+
+async function transformPublicKeyToAccountId(key: string) {
+    const protoBufPrefix = `1220`;
+    const bufferRaw = Buffer.from(`${protoBufPrefix}${key}`, 'hex');
+    return '0.0.' + base32.stringify(bufferRaw, { pad: false });
+}
