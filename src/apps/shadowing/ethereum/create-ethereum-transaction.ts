4f780f21b6131b972032e0a01038053fcbb0316e
kamil chmielewski <kamil.chmielewski@arianelabs.com>
2024-10-10 15:26:58 +0200

    test commit

--- src/apps/shadowing/ethereum/create-ethereum-transaction.ts
+++ src/apps/shadowing/ethereum/create-ethereum-transaction.ts
@@ -4,27 +4,26 @@ import {
 	Client,
 	EthereumTransaction,
 	Hbar,
-	PrivateKey,
 	TransactionId,
 } from '@hashgraph/sdk';
+import { sendHbarToAlias } from '@/apps/shadowing/transfers/send-hbar-to-alias';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const OPERATOR_PRIVATE = process.env.OPERATOR_PRIVATE;
 
-export async function createEthereumTransaction(
-	transactionData: { txHash: string; gas: number },
-	accountId: AccountId,
-	client: Client
-) {
-	const rawBody = await getRawTransaction(transactionData.txHash);
+export async function createEthereumTransaction(transactionData: {txHash: string, evmAddress: string, hbar: number, gas: number}, accountId: AccountId, client: Client) {
+	const rawBody = await getRawTransaction(
+		transactionData.txHash
+	);
+
+	await sendHbarToAlias(accountId, transactionData.evmAddress, transactionData.hbar, client);
 	const txId = TransactionId.generate(accountId);
-	const transaction = await new EthereumTransaction()
+	const transaction = new EthereumTransaction()
 		.setTransactionId(txId)
 		.setEthereumData(Uint8Array.from(Buffer.from(rawBody.substring(2), 'hex')))
 		.setMaxGasAllowanceHbar(new Hbar(transactionData.gas))
 		.freezeWith(client)
-		.sign(PrivateKey.fromString(String(OPERATOR_PRIVATE)));
 
 	const txResponse = await transaction.execute(client);
 
--- src/apps/shadowing/index.ts
+++ src/apps/shadowing/index.ts
@@ -1,23 +1,27 @@
-import {
-	getAllFrontierData,
-	getAllGenesisData,
-} from '@/apps/shadowing/frontier/get-all-frontier-data';
-import { Client, AccountId } from '@hashgraph/sdk';
+import {Client, AccountId, TransactionId, EthereumTransaction, Hbar} from '@hashgraph/sdk';
 import dotenv from 'dotenv';
-import { startNetworkReplicationProcess } from '@/apps/shadowing/blockchain-utils/start-network-replication-process';
 dotenv.config();
 const OPERATOR_PRIVATE = process.env.OPERATOR_PRIVATE;
 
 const HARDCODED_NUMBER_OF_BLOCKS = 100000;
 const HARDCODED_BLOCK_NUMBER_WITH_TRANSACTIONS = 5966639;
-const accountsMapping = getAllFrontierData();
+const HARDCODED_RAW_BODY = '0xf86b048503ff9aca0782520f94e64fac7f3df5ab44333ad3d3eb3fb68be43f2e8c830fffff808401546d71a026cf0758fda122862a4de71a82a3210ef7c172ee13eae42997f5d32b747ec78ca03587c5c2eee373b1e45693544edcde8dde883d2be3e211b3f0f3c840d6389c8a';
 
 const node = { '127.0.0.1:50211': new AccountId(3) };
-const genesisTransactions = getAllGenesisData();
 const client = Client.forNetwork(node).setMirrorNetwork('127.0.0.1:5600');
 const accountId = new AccountId(2);
 client.setOperator(accountId, OPERATOR_PRIVATE || '');
 
 (async () => {
-	await startNetworkReplicationProcess(accountId, genesisTransactions, client);
+
+	const txId = TransactionId.generate(accountId);
+	const transaction = new EthereumTransaction()
+		.setTransactionId(txId)
+		.setEthereumData(Uint8Array.from(Buffer.from(HARDCODED_RAW_BODY.substring(2), 'hex')))
+		.setMaxGasAllowanceHbar(new Hbar(100))
+		.freezeWith(client)
+
+	const txResponse = await transaction.execute(client);
+
+	console.log('txResponse', txResponse.toJSON());
 })();
