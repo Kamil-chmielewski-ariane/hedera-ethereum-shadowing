4ac9b9b9ba70d092f7af627d96c9495a1be06c78
michal.myslinski <michal.myslinski@arianelabs.com>
2024-10-25 15:43:33 +0200

    platform not active test

--- src/apps/issues/platfrom-not-active-error.ts
+++ src/apps/issues/platfrom-not-active-error.ts
@@ -11,6 +11,10 @@ export async function replicatePlatformNotActiveError(
 ) {
 
     for(let i = 0; true; i++) {
+		// TRANSACTION THAT WILL NOT GIVE FREEZE ERROR AND SHOULD BE PROPER
+		// await sendHbarToAliasProper(accountId, txSenderAddress, client);
+
+		// TRANSACTION THAT WILL KEEP FAILING AFTER SOME TIME
         await sendHbarToAlias(
             accountId,
             txSenderAddress,
@@ -18,6 +22,7 @@ export async function replicatePlatformNotActiveError(
             client,
 			i
         );
+		// TODO
         // await createEthereumTransaction("0x7afaa1366e0a6273b29a6d8d7c932e939dc681ef70dfdeed2fd1a66f582d000c", client, operatorPrivate)
     }
 }
@@ -35,35 +40,56 @@ async function sendHbarToAlias(
 			.addHbarTransfer(accountId, new Hbar(amountHBar).negated())
 			.addHbarTransfer(evmAddress, new Hbar(amountHBar));
 
-		// Execute the transaction
 		const response = await transaction.execute(client);
         console.log(await response.getReceipt(client));
-		// TODO: uncomment when receipt API is ready
-		// sendTransactionInfoToReceiptApi(txId, evmAddress, currentBlock, "TRANSFER_TRANSACTION", txTimestamp);
 	} catch (error) {
 		console.error('Error sending HBAR to alias:', error);
 		await writeLogFile(`logs/iterator-for-transfer-transaction.json: `, `${iterator.toString()} \n`, );
 	}
 }
 
-async function createEthereumTransaction(
-	txHash: string,
-	client: Client,
-	operatorPrivate: string
+// async function createEthereumTransaction(
+// 	txHash: string,
+// 	client: Client,
+// 	operatorPrivate: string
+// ) {
+// 	try {
+// 		const rawBody = await getRawTransaction(txHash);
+// 		const transaction = await new EthereumTransaction()
+// 			.setEthereumData(Uint8Array.from(Buffer.from(rawBody.substring(2), 'hex')))
+// 			.setMaxGasAllowanceHbar(new Hbar(21000))
+// 			.sign(PrivateKey.fromString(String(operatorPrivate)));
+// 		const txResponse = await transaction.execute(client);
+
+// 		console.log('txResponse', txResponse.toJSON());
+//         console.log(await txResponse.getReceipt(client));
+// 	} catch (error) {
+//         console.log("error ", error)
+// 	}
+// }
+
+export async function sendHbarToAliasProper(
+	accountId: AccountId,
+	evmAddress: string,
+	client: Client
 ) {
 	try {
-		const rawBody = await getRawTransaction(txHash);
-		const transaction = await new EthereumTransaction()
-			.setEthereumData(Uint8Array.from(Buffer.from(rawBody.substring(2), 'hex')))
-			.setMaxGasAllowanceHbar(new Hbar(21000))
-			.sign(PrivateKey.fromString(String(operatorPrivate)));
-		const txResponse = await transaction.execute(client);
+		console.log(`Running transaction ${accountId}, ${evmAddress}`);
+		const txId = TransactionId.generate(accountId);
+		const transaction = new TransferTransaction()
+			.addHbarTransfer(accountId, new Hbar(100).negated())
+			.addHbarTransfer(evmAddress, new Hbar(100))
+			.setTransactionId(txId)
+			.setNodeAccountIds([new AccountId(3)])
+			.freeze();
 
-		console.log('txResponse', txResponse.toJSON());
-        console.log(await txResponse.getReceipt(client));
+		// Execute the transaction
+		await new Promise(resolve => setTimeout(resolve, 1));
+		await transaction.execute(client);
+		const txTimestamp = new Date().toISOString();
 		// TODO: uncomment when receipt API is ready
-		// sendTransactionInfoToReceiptApi(txId, accountTo, currentBlock, "ETHEREUM_TRANSACTION", txTimestamp);
+		// sendTransactionInfoToReceiptApi(txId, evmAddress, currentBlock, "TRANSFER_TRANSACTION", txTimestamp);
 	} catch (error) {
-        console.log("error ", error)
+		console.error('Error sending HBAR to alias:', error);
 	}
 }
