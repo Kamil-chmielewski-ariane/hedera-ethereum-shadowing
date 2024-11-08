cf271994e0b13f543cd5cc645a333d22e99d2c98
Kamil Chmielewski <kamil.chmielewski@arianelabs.com>
2024-11-08 11:01:31 +0100

    added config to ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,11 @@
 module.exports = {
     apps : [{
         name   : "hedera-shadowing",
-        script : "./src/apps/shadowing/index.ts"
+        script : "./src/apps/shadowing/index.ts",
+        error_file  : "./logs/hedera-shadowing-errors-full.log",
+        out_file : "./logs/hedera-shadowing-out-full.log",
+        log_date_format: "YYYY-MM-DD HH:mm",
+        max_memory_restart: '2G',
+        autorestart: false
     }]
 }
