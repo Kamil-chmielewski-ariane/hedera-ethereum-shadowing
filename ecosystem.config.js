16623f8e331be7d8ee1ed0a1bd00ce4dcde7d7ed
Kamil Chmielewski <kamil.chmielewski@arianelabs.com>
2024-11-15 14:40:24 +0100

    changed interpreter for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,13 @@
 module.exports = {
     apps : [{
         name   : "hedera-shadowing",
-        script : "./src/apps/shadowing/index.ts",
+        script : "pnpm",
         error_file  : "./logs/hedera-shadowing-errors-full.log",
         out_file : "./logs/hedera-shadowing-out-full.log",
         log_date_format: "YYYY-MM-DD HH:mm",
         max_memory_restart: '2G',
         autorestart: false,
+        interpreter: "none",
+        args: "ts-node -r tsconfig-paths/register src/apps/shadowing/index.ts",
     }]
 }
